259a8a5c12ee311e975298a25910bb2c3bf08e34
KevinFlorenciaMoragas <120371012+KevinFlorenciaMoragas@users.noreply.github.com>
2023-04-26 20:20:10 +0200

    26/04

---
+++ juego/src/Game.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import options from "./Options";
+import getResult from "./Results";
+import OptionButton from "./OptionsButton";
+import useChoices from "./UseChoices";
+export default function Game() {
+  const {
+    userChoice,
+    computerChoice,
+    userMessage,
+    computerMessage,
+    result,
+    disabled,
+    handlePlay,
+    reset,
+  } = useChoices();
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-800">
+      <div className="rounded-lg p-4 bg-gray-500">
+        <h1 className="text-3xl mb-4 text-center font-bold">¡A jugar!</h1>
+        <div className="max-w-md mx-auto">
+          {options.map((option) => (
+            <OptionButton
+              key={option.id}
+              option={option}
+              handlePlay={handlePlay}
+              disabled={disabled}
+            />
+          ))}
+          {userChoice !== null && <p className="text-xl mt-4">{userMessage}</p>}
+          {computerChoice !== null && (
+            <p className="text-xl mt-4">{computerMessage}</p>
+          )}
+          {result !== null && (
+            <div className="mt-8">
+              {result === 0 && <p className="text-xl mt-4">🤷🏽‍♀️ Empate</p>}
+              {result === 1 && (
+                <p className="text-xl mt-4">
+                  ✅ Has ganado con {options[userChoice]?.name} contra{" "}
+                  {options[computerChoice]?.name}
+                </p>
+              )}
+              {result === 2 && (
+                <p className="text-xl mt-4">
+                  ❌ Has perdido con {options[userChoice]?.name} contra{" "}
+                  {options[computerChoice]?.name}
+                </p>
+              )}
+              <button
+                className="bg-yellow-500 hover:bg-yellow-700 text-black font-semibold py-2 px-4 mt-4 border-b-4 border-yellow-700"
+                onClick={reset}
+              >
+                Jugar de nuevo
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
